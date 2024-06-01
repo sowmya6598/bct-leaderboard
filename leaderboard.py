24d9ba20d8edf5b3730a05e572b8711a30b27bf9
Sowmya Thottambeti <sowmya6598@gmail.com>
2024-05-31 20:21:55 -0400

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
-# read the excel file
+# read the Excel file
 df = pd.read_excel('week1.xlsx')
 
 # list of emails to exclude
-exclude_emails = ['mj', 'ashleigh', 'tija']
+exclude_emails = ['mj', 'ashleigh']
+
+# extract the time part from 'submitted_on'
+df['submitted_time'] = pd.to_datetime(df['submitted_on']).dt.time
 
 # filter out the excluded emails and select only the rows where the answer is correct, then sort by submission time
-dataframe = df[(df['correct'] == "Yes") & (~df['email'].isin(exclude_emails))].sort_values(by='submitted_on')
+dataframe = df[(df['correct'] == "Yes") & (~df['email'].isin(exclude_emails))].sort_values(by='submitted_time')
 
-# initialize a tracker for correct answers and a result list for the first 10 participants to reach 4 correct answers
+# initialize a tracker for correct answers and a result list for the first 10 participants to reach 5 correct answers
 tracker = {}
 result = []
 
@@ -24,7 +27,7 @@ for index, row in dataframe.iterrows():
         tracker[email] = 1
     
     # check if the participant reached 5 correct answers
-    if tracker[email] == 4:
+    if tracker[email] == 5:
         result.append(email)
         
         # stop if we have 10 participants
